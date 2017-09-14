aad43b1f3e47f239e1e074d6d45c8994bc0f31a0
Clara Nobre <aclaranobre@gmail.com>
2017-09-14 10:25:52 -0300

    Adicionado modelo phong e light

--- Ray Tracer/includes/phong.h
+++ Ray Tracer/includes/phong.h
@@ -16,7 +16,7 @@ public:
 bool phong::hit(virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const){
   case kPhong:
   {
-    Vec3f diffuse = 0, specular = 0;
+    vec3 diffuse = 0, specular = 0;
       for (uint32_t i = 0; i < lights.size(); ++i) {
         vec3 lightDir, lightIntensity;
         IsectInfo isectShad;
@@ -25,14 +25,14 @@ bool phong::hit(virtual bool hit(const ray& r, float tmin, float tmax, hit_recor
     bool vis = !trace(hitPoint + hitNormal * options.bias, -lightDir, objects, isectShad, kShadowRay);
 
     // compute the diffuse component
-    diffuse += vis * isect.hitObject->albedo * lightIntensity * std::max(0.f, hitNormal.dot(-lightDir));
+    diffuse += vis * isect.hit->albedo * lightIntensity * std::max(0.f, hitNormal.dot(-lightDir));
 
     // compute the specular component
     // what would be the ideal reflection direction for this light ray
     vec3 R = reflect(lightDir, hitNormal);
-    specular += vis * lightIntensity * std::pow(std::max(0.f, R.dot(-dir)), isect.hitObject->n);
+    specular += vis * lightIntensity * std::pow(std::max(0.f, R.dot(-dir)), isect.hit->n);
     }
-    hitColor = diffuse * isect.hitObject->Kd + specular * isect.hitObject->Ks;
+    hitColor = diffuse * isect.hit->Kd + specular * isect.hit->Ks;
     //std::cerr << hitColor << std::endl;
     break;
   }
